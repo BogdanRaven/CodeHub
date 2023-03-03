45d8d3d87bb3c00d3030f40e99f05665a4cb3ec2
BogdanMasliy <masliy45@gmail.com>
2023-03-03 17:18:18 +0200

    upload files

---
+++ GameMechanics/Interfaces/IMovementService.cs
@@ -0,0 +1,10 @@
+﻿using CodeHub.GameData;
+
+namespace CodeHub.GameMechanics
+{
+    public interface IMovementService
+    {
+        public void MoveObject(IMovable objMovable, IField field);
+        public void MoveObjectWithReflection(IMovable objMovable, TileType borderTile, IField field);
+    }
+}
